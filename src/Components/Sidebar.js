eac891de2897ece8a308f54f3eefa29b5768b951
RKRedfern <rkredfern@gmail.com>
2021-06-10 12:29:59 -0400

    react icons / styled components installed

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 const Sidebar = () => {
     return(
         <div>
-
+            <h1> Sidebar </h1>
         </div>
     )
 }
